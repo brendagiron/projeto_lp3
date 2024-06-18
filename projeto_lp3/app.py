2a65187ce6f229f2bee70fe315feec515260e8d4
brendagiron <brendagironbarbosa2007@gmail.com>
2024-06-18 09:04:02 -0300

    Aula sobre templates

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from validate_docbr import CPF, CNPJ
 
+cnpj = CNPJ()
+cpf = CPF()
 #app = Flask('Minha app')
 app = Flask(__name__)
 
@@ -7,18 +10,36 @@ app = Flask(__name__)
 
 # / - home page
 @app.route('/')
-
-def home ():
-    return '<h1> Home Page </h1>'
+def home():
+    return render_template("home.html")
 
 #/contato - página de contato
 
 @app.route('/contato')
 def contato():
-    return '<h1>Contato</h1>'
+    return render_template("contato.html")
 
 #/produtos - pagina de produtos
 
 @app.route('/produtos')
 def produtos():
-    return '<h1>Produtos</h1>'
+    lista_produtos = [
+        {'nome': 'chic', 'descricao': 'anita'},
+        {'nome': 'astral', 'descricao': 'risque'},
+        {'nome': 'dengo', 'descricao': 'impala'}
+    ]
+    return render_template("produtos.html", produtos = lista_produtos)
+
+@app.route('/servicos')
+def servicos():
+    return "<h1> Nossos serviços </h1>"
+
+@app.route("/gerar-cpf")
+def gerar_cpf():
+    return f"CPF: {cpf.generate(True)}"
+
+@app.route("/gerar-cnpj")
+def gerar_cnpj():
+    return f"CNPJ: {cnpj.generate(True)}"
+
+
